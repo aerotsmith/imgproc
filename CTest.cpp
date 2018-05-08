951e71d65cc009281ddec6741b74254576f3b681
Tom Smith <tesmith.573@gmail.com>
2018-05-07 19:41:17 -0500

    Added CLI interface

--- CTest.cpp
+++ CTest.cpp
@@ -1,9 +1,53 @@
 #include <iostream>
 #include "ImgUtils.h"
 #include "opencv2/opencv.hpp"
+#include <dirent.h>
+#include <errno.h>
+#include <iostream>
 
 using namespace std;
 using namespace cv;
+
+int getdir (string dir)
+{
+    DIR *dp;
+    struct dirent *dirp;
+    if ((dp  = opendir(dir.c_str())) == NULL)
+    {
+        cout << "Error(" << errno << ") opening " << dir << endl;
+        return errno;
+    }
+
+    while ((dirp = readdir(dp)) != NULL)
+    {
+        if (dirp->d_name[0]!='.')
+            cout << dirp->d_name << "  ";
+    }
+    cout << endl;
+
+    closedir(dp);
+    return 0;
+}
+
+void getParams(string str, int *x, int *y, float *tol)
+{
+    stringstream ss;    
+ 
+    ss << str;
+ 
+    string temp;
+
+    ss >> temp;
+    stringstream(temp) >> *x;
+    ss >> temp;
+    stringstream(temp) >> *x;
+    ss >> temp;
+    stringstream(temp) >> *y;
+    ss >> temp;
+    stringstream(temp) >> *tol;
+
+    return;
+}
 
 int main(int argc, char **argv)
 {
@@ -13,61 +57,126 @@ int main(int argc, char **argv)
     float tolerance;
     vector<xy> rv;
     vector<vector<xy>> perimeters;
-
-    if (argc != 5)
-    {
-        cerr << "usage: ctest <Image_Path> <x-coord> <y-coord> <tolerance>\n";
-        return -1;
-    }
-
-    // Collect parameters
-    x = atoi(argv[2]);
-    y = atoi(argv[3]);
-    tolerance = (float)atof(argv[4]);
-
     string path="../Images/";
-
-    Mat image;
-    image = imread(path.append(argv[1]), 1);
-
-    if (!image.data)
-    {
-        cout << "No image data (warning: OpenCV recognize files by extensions)\n";
-        return -1;
-    }
+    string filename;
 
     ImgUtils imgUtils;
-
-    imgUtils.displayImage(argv[1],image);
-
-    // find the region at x,y and transform region bitmap into a perimeter bitmap
-    region = imgUtils.findRegion(&image,x,y,tolerance, &rv);
-    perimeter = imgUtils.findPerimeter(region, image.rows, image.cols, &perimeters);
-
-/*
-    vector<vector<xy>>::iterator it;
-    vector<xy>::iterator it2;
-    for (it = perimeters.begin(); it != perimeters.end(); it++)
-    {
-        cout << "New perimeter\n";
-        for (it2=it->begin(); it2!=it->end(); it2++)
-        {
-            cout << it2->x << ", "<<it2->y<<endl;
-        }
-    }
-*/
-
-    // Display region and perimeter bitmaps
-    imgUtils.displayPixels("Region", region, image.rows, image.cols);
-    imgUtils.displayPixels("Perimeter", perimeter, image.rows, image.cols);
-
-    imgUtils.savePixels(region, image.rows, image.cols, "regionimg.png");
-    imgUtils.savePixels(perimeter, image.rows, image.cols, "perimeterimg.png");
-
+    Mat image;
     Mat smoothPerimeters;
-    smoothPerimeters = imgUtils.findSmoothPerimeter(&perimeters, image.rows, image.cols);
 
-    imgUtils.displayImage("Smooth Perimeters", smoothPerimeters);
+    bool imgLoaded=false;
+    bool regionCreated=false;
+    bool perimeterCreated=false;
+    bool smoothCreated=false;
+
+
+    string cmd="";
+
+    while (cmd!="quit")
+    {
+        cout << "\n\nCommands:\n";
+        cout << "list - lists image files\n";
+        cout << "load <image_name> - load an image\n";
+        if (imgLoaded)
+           cout << "region <x> <y> <tolerance> - fills region in the loaded image at x,y and tolerance (0.1 is good)\n";
+        if (regionCreated)
+        {
+           cout << "save region    - save region as image and text file\n";
+           cout << "perimeter      - finds the perimeter of the region\n";
+        }
+        if (perimeterCreated)
+        {
+           cout << "save perimeter - save perimeter as image and text file\n";
+           cout << "smooth         - finds the smooth perimeter of the region\n";
+        }
+        cout << "type quit to exit\n\n";
+
+        getline(cin,cmd);
+
+        if (cmd=="list")
+           getdir(path);
+        else if ((cmd.compare(0,5,"load "))==0)
+        {
+            filename=cmd.substr (5,cmd.length()-5);
+            cout << "Loading "<<filename<<"\n";
+            cout << "Click image and hit key to continue...\n";
+            image = imread(path.append(filename), 1);
+            if (!image.data)
+            {
+                cout << "No image data (warning: OpenCV recognize files by extensions)\n";
+                return -1;
+            }
+
+            imgUtils.displayImage(path.append(filename),image);
+
+            imgLoaded=true;
+        }
+        else if ((imgLoaded) && ((cmd.compare(0,7,"region "))==0))
+        {
+            getParams(cmd, &x,&y, &tolerance);
+            
+            cout << "Flooding image at "<<x<<", "<<y<<endl;
+            region = imgUtils.findRegion(&image,x,y,tolerance, &rv);
+            cout << "Click image and hit key to continue...\n";
+            imgUtils.displayPixels("Region", region, image.rows, image.cols);
+
+            regionCreated=true;
+        }
+        else if ((regionCreated) && ((cmd.compare(0,9,"perimeter"))==0))
+        {
+            cout << "Finding perimeter\n";
+            perimeter = imgUtils.findPerimeter(region, image.rows, image.cols, &perimeters);
+            cout << "Click image and hit key to continue...\n";
+            imgUtils.displayPixels("Perimeter", perimeter, image.rows, image.cols);
+
+            perimeterCreated=true;
+        }
+        else if ((regionCreated) && ((cmd.compare(0,11,"save region"))==0))
+        {
+            cout << "Writing regionimg.png and region.txt\n";
+            imgUtils.savePixels(region, image.rows, image.cols, "regionimg.png");
+
+            ofstream file;
+            file.open ("region.txt");
+
+            vector<xy>::iterator it;
+            for (it=rv.begin(); it!=rv.end(); it++)
+            {
+                file << it->x << ", "<<it->y<<endl;
+            }
+            file.close();
+        }
+        else if ((perimeterCreated) && ((cmd.compare(0,6,"smooth"))==0))
+        {
+            cout << "Finding smooth perimeter\n";
+            smoothPerimeters = imgUtils.findSmoothPerimeter(&perimeters, image.rows, image.cols);
+            cout << "Click image and hit key to continue...\n";
+            imgUtils.displayImage("Smooth Perimeters", smoothPerimeters);
+
+            smoothCreated = true;
+        }
+        else if ((perimeterCreated) && ((cmd.compare(0,14,"save perimeter"))==0))
+        {
+            imgUtils.savePixels(perimeter, image.rows, image.cols, "perimeterimg.png");
+            cout << "Writing perimeterimg.png and perimeter.txt\n";
+
+            ofstream file;
+            file.open ("perimeter.txt");
+
+            vector<vector<xy>>::iterator it;
+            vector<xy>::iterator it2;
+            for (it = perimeters.begin(); it != perimeters.end(); it++)
+            {
+                file << "New perimeter\n";
+                for (it2=it->begin(); it2!=it->end(); it2++)
+                {
+                    file << it2->x << ", "<<it2->y<<endl;
+                }
+            }
+            file.close();
+        }
+        
+    }
 
     // Clean up region
     for (int i=0; i<image.rows; i++)
--- README.md
+++ README.md
@@ -8,10 +8,17 @@ cmake ..
 make
 
 To run:
-ctest Image/test1.png 135 60 0.01
+ctest test1.png 135 60 0.01
 
 
 REFERENCES:
 8-Way Recursive Method:
 https://www.codeproject.com/Articles/6017/QuickFill-An-efficient-flood-fill-algorithm
+
+RGB 2 HSV conversion:
+https://www.rapidtables.com/convert/color/rgb-to-hsv.html
+
+Distance between colors calculation:
+https://stackoverflow.com/questions/35113979/calculate-distance-between-colors-in-hsv-space
+
 
